ec604d4f6cd5687b5285d752ce7ad58f4cbdd846
filipforsstrom <filipforsstrom@gmail.com>
2023-06-04 15:49:25 +0200

    0.0.0 position and random notes

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,44 @@
 //! Renders a 2D scene containing a single, moving sprite.
 
 use bevy::{prelude::*, window::PrimaryWindow};
+use rand::random;
 
 const PLAYHEAD_SPEED: f32 = 500.0;
+const NUMBER_OF_RANDOM_NOTES: usize = 10;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_startup_system(spawn_camera)
         .add_startup_system(spawn_playhead)
+        .add_startup_system(spawn_notes)
         .add_system(playhead_movement)
         .run();
 }
 
 pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
-    // let windows = window_query.get_single().unwrap();
+    let windows = window_query.get_single().unwrap();
 
-    // commands.spawn(Camera2dBundle {
-    //     transform: Transform::from_xyz(windows.width() / 2.0, windows.height() / 2.0, 100.0),
-    //     ..default()
-    // });
+    commands.spawn(Camera2dBundle {
+        transform: Transform::from_xyz(windows.width() / 2.0, windows.height() / 2.0, 100.0),
+        ..default()
+    });
 
     // commands.spawn(Camera2dBundle::default());
 
-    commands.spawn(Camera2dBundle {
-        transform: Transform::from_xyz(0.0, 0.0, 999.9),
-        ..default()
-    });
+    // commands.spawn(Camera2dBundle {
+    //     transform: Transform::from_xyz(0.0, 0.0, 999.9),
+    //     ..default()
+    // });
 }
 
 #[derive(Component)]
 pub struct Playhead {
+    pub position: Vec2,
+}
+
+#[derive(Component)]
+pub struct Note {
     pub position: Vec2,
 }
 
@@ -46,10 +54,10 @@ pub fn spawn_playhead(
         .spawn(SpriteBundle {
             sprite: Sprite {
                 color: Color::rgb(1., 0., 0.),
-                custom_size: Some(Vec2::new(5.0, window.height())),
+                custom_size: Some(Vec2::new(5.0, window.height() * 2.)),
                 ..default()
             },
-            transform: Transform::from_translation(Vec3::new(window.width() / -2., 0., 0.)),
+            transform: Transform::from_translation(Vec3::new(0., 0., 0.)),
             ..Default::default()
         })
         .insert(Playhead {
@@ -68,8 +76,35 @@ pub fn playhead_movement(
         let position = Vec3::new(playhead.position.x, playhead.position.y, 0.0);
         transform.translation += position * PLAYHEAD_SPEED * time.delta_seconds();
 
-        if transform.translation.x > window.width() / 2. {
-            transform.translation.x = window.width() / -2.;
+        if transform.translation.x > window.width() {
+            transform.translation.x = 0.;
         }
+    }
+}
+
+pub fn spawn_notes(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+) {
+    let window = window_query.get_single().unwrap();
+
+    for _ in 0..NUMBER_OF_RANDOM_NOTES {
+        let random_x = random::<f32>() * window.width();
+        let random_y = random::<f32>() * window.height();
+
+        commands
+            .spawn(SpriteBundle {
+                sprite: Sprite {
+                    color: Color::rgb(0., 1., 0.),
+                    custom_size: Some(Vec2::new(10., 10.)),
+                    ..default()
+                },
+                transform: Transform::from_xyz(random_x, random_y, 0.0),
+                ..default()
+            })
+            .insert(Note {
+                position: Vec2::new(1.0, 0.0),
+            });
     }
 }
